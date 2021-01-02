8590485be948ef74e59491e5fcfc53109ed996bd
Joakim31xd <73369377+Joakim31xd@users.noreply.github.com>
2021-01-01 23:11:41 -0500

    Commit Union -Planetas

--- Presentable_1/src/main.cpp
+++ Presentable_1/src/main.cpp
@@ -28,6 +28,7 @@
 #include <cstdlib>
 #include <math.h>
 using namespace std;
+using namespace cv;
 
 
 /************************* dictionary:
@@ -55,9 +56,18 @@ int main(int argc, char **argv)
 	cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters::create();
 
 	int dict_number=6;
-	vector<int> ArrayIDdelSol;
+	vector<int> ArrayIDdelSol; //marcadores hechos para el sol
+	vector<int> ArrayIDdeTierra; //marcadores para elplaneta Tierra
+
+
 	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dict_number));
 	cv::Ptr<cv::aruco::GridBoard> boardSol = cv::aruco::GridBoard::create(5, 5, 0.04, 0.01, dictionary);
+
+
+	cv::Ptr<cv::aruco::GridBoard> boardTierra = cv::aruco::GridBoard::create(5, 5, 0.04, 0.01, dictionary);
+
+
+
 	cv::Mat boardImage;
 	for(int j=0;j<25;j++){
 		ArrayIDdelSol.push_back(j); //Asigno los ids para un arreglo
@@ -65,60 +75,81 @@ int main(int argc, char **argv)
 	for(int i=0;i<25;i++){
 		boardSol->ids[i]=ArrayIDdelSol.at(i); //para cada posicion del board asigno los valores del vector
 	}
+
+
+	//Para la Tierra
+	cv::Mat boardImageT;
+		for(int k=0;k<25;k++){
+			ArrayIDdeTierra.push_back(k+25); //Asigno los ids para un arreglo de la tierra
+		}
+		for(int l=0;l<25;l++){
+			boardTierra->ids[l]=ArrayIDdeTierra.at(l); //para cada posicion del board asigno los valores del vectorTierra
+		}
+
+
+	//Dibujar MArcadores
 	boardSol->draw( cv::Size(600,500), boardImage, 10, 1 );
+	boardTierra->draw( cv::Size(600,500), boardImageT, 10, 1 ); //Para la tierra
+
+
+	//Impresion de imagenes
 	cv::imwrite("MarcadordelSol.png", boardImage);
+	cv::imwrite("MarcadordeTierra.png", boardImageT); //Para la tierra
+
 
 	//ENCENDIDO DE LA CAMARA
-	int wait_time = 10;
-	int id_camera = 0; // 0 = camera default, 1 = second camera
-	// IP DE CAMARA DE CELULAR CON LA APP DROIDCAM
-	const std::string videoStreamAddress="http://192.168.1.5:4747/video";
-	cv::VideoCapture in_video;
-	double fx, fy, cx, cy, k1, k2, k3, p1, p2;
-	fx = 955.8925;
-	fy = 955.4439;
-	cx = 296.9006;
-	cy = 215.9074;
-	k1 = -0.1523;
-	k2 = 0.7722;
-	k3 = 0;
-	p1 = 0;
-	p2 = 0;
-	cv::Mat cameraMatrix = (cv::Mat_<float>(3, 3) <<
-		fx, 0.0, cx,
-		0.0, fy, cy,
-		0.0, 0.0, 1.0);
-	cv::Mat distCoeffs = (cv::Mat_<float>(5, 1) << k1, k2, p1, p2, k3);
-	in_video.open(id_camera); //CAMARA DE LA LAPTOP CONECTADA POR USB
-	//in_video.open(videoStreamAddress); // CAMARA DEL CELULAR CON DIRECCION IP
-	if (!in_video.isOpened()) {
-		std::cerr << "failed to open camera (id=" << id_camera<<")." << std::endl;
-		return 1;
-	}
-	while (in_video.grab()) {
-		cv::Mat image, image_copy;
-		in_video.retrieve(image);
-		image.copyTo(image_copy);
-		std::vector<int> ids;
-		std::vector<std::vector<cv::Point2f>> corners;
-		cv::aruco::detectMarkers(image, dictionary, corners, ids);
+		int wait_time = 10;
+		int id_camera = 0; // 0 = camera default, 1 = second camera
+		// IP DE CAMARA DE CELULAR CON LA APP DROIDCAM
+		//const std::string videoStreamAddress="http://192.168.1.5:4747/video";
+		cv::VideoCapture in_video;
+		double fx, fy, cx, cy, k1, k2, k3, p1, p2;
+		fx = 955.8925;
+		fy = 955.4439;
+		cx = 296.9006;
+		cy = 215.9074;
+		k1 = -0.1523;
+		k2 = 0.7722;
+		k3 = 0;
+		p1 = 0;
+		p2 = 0;
+		cv::Mat cameraMatrix = (cv::Mat_<float>(3, 3) <<
+			fx, 0.0, cx,
+			0.0, fy, cy,
+			0.0, 0.0, 1.0);
+		cv::Mat distCoeffs = (cv::Mat_<float>(5, 1) << k1, k2, p1, p2, k3);
 
-		// If at least one marker detected
-		if (ids.size() > 0){
-			cv::aruco::drawDetectedMarkers(image_copy, corners, ids);
-			cv::Vec3d rvec, tvec;
-			int valid =cv::aruco::estimatePoseBoard(corners, ids, boardSol, cameraMatrix, distCoeffs, rvec, tvec, false);
-			if(valid > 0)
-				cv::aruco::drawAxis(image_copy, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+		//Vectores de rotacion y traslacion
+		Mat rota;
+		Mat trasla;
+
+		in_video.open(id_camera); //CAMARA DE LA LAPTOP CONECTADA POR USB
+		//in_video.open(videoStreamAddress); // CAMARA DEL CELULAR CON DIRECCION IP
+		if (!in_video.isOpened()) {
+			std::cerr << "Fallo al abrir camara (id=" << id_camera<<")." << std::endl;
+			return 1;
 		}
-		imshow("Aruco Marker Detected", image_copy);
-		char key = (char)cv::waitKey(wait_time);
-		if (key == 27) // ascii 27 = ESC
-			break;
-	}
+		while (in_video.grab()) {
+			cv::Mat image, image_copy;
+			in_video.retrieve(image);
+			image.copyTo(image_copy);
+			std::vector<int> ids;
+			std::vector<std::vector<cv::Point2f>> corners;
+			cv::aruco::detectMarkers(image, dictionary, corners, ids);
 
-	in_video.release();
-
-
+			// If at least one marker detected
+			if (ids.size() > 0){
+				cv::aruco::drawDetectedMarkers(image_copy, corners, ids);
+				cv::Vec3d rvec, tvec;
+				int valid =cv::aruco::estimatePoseBoard(corners, ids, boardSol, cameraMatrix, distCoeffs, rvec, tvec, false);
+				if(valid > 0)
+					cv::aruco::drawAxis(image_copy, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+			}
+			imshow("Aruco Marker Detected", image_copy);
+			char key = (char)cv::waitKey(wait_time);
+			if (key == 27) // ascii 27 = ESC
+				break;
+		}
+		in_video.release();
     return 0;
 }
